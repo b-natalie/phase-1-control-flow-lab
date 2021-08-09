06ccf846c5241b129c6a302717b23ff8f60b1f93
Natalie Barba <nbarba2010@gmail.com>
2021-08-09 14:52:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let msg;
+
+  if (distance > 2500) {
+    msg = 'No can do.';
+  } else if (distance > 2000) {
+    msg = 'I will gladly take your thirty bucks.';
+  } else {
+    msg = 'This one is on me!';
+  }
+
+  return msg;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let msg;
+  city === 'NYC' ? (msg = 'Ok, sounds good.') : (msg = 'No go.');
+  return msg;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  let msg;
+  switch (generosity) {
+    case 'generous' :
+      msg = 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      msg = 'Thank you.';
+      break;
+    default:
+      msg = 'Bye.';
+  }
+  return msg;
 }
